3af23e37375ba0bd89e2327f6844f8d3699be869
Lis3ee <godard.lise@gmail.com>
2024-02-28 16:25:03 +0100

    Auth

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -4,6 +4,10 @@ import Route from "./Route.js";
 export const allRoutes = [
     new Route("/", "Accueil", "/Pages/home.html"),
     new Route("/galerie", "La galerie", "/Pages/galerie.html"),
+    new Route("/signin", "Connexion", "/Pages/auth/signin.html"),
+    new Route("/signup", "Inscription", "/Pages/auth/signup.html"),
+    new Route("/account", "Mon compte", "/Pages/auth/account.html"),
+    new Route("/editPassword", "Modifier mot de passe", "/Pages/auth/editPassword.html"),
 ];
 
 //Le titre s'affiche comme ceci : Route.titre - websitename
