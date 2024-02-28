5a86a88a72d0e245f81afea5bdd10c1f232161a0
Lis3ee <godard.lise@gmail.com>
2024-02-28 18:51:21 +0100

    Reservations

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -8,6 +8,8 @@ export const allRoutes = [
     new Route("/signup", "Inscription", "/Pages/auth/signup.html"),
     new Route("/account", "Mon compte", "/Pages/auth/account.html"),
     new Route("/editPassword", "Modifier mot de passe", "/Pages/auth/editPassword.html"),
+    new Route("/allResa", "Vos réservations", "/Pages/reservations/allResa.html"),
+    new Route("/reserver", "Réserver", "/Pages/reservations/reserver.html"),
 ];
 
 //Le titre s'affiche comme ceci : Route.titre - websitename
