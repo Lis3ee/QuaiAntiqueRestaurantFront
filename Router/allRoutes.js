eab8283e241b72699e689f6877de507e501ee22c
Lis3ee <godard.lise@gmail.com>
2024-03-01 15:25:23 +0100

    User

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -2,14 +2,14 @@ import Route from "./Route.js";
 
 //Définir ici vos routes
 export const allRoutes = [
-    new Route("/", "Accueil", "/Pages/home.html"),
-    new Route("/galerie", "La galerie", "/Pages/galerie.html"),
-    new Route("/signin", "Connexion", "/Pages/auth/signin.html"),
-    new Route("/signup", "Inscription", "/Pages/auth/signup.html", "/js/auth/signup.js"),
-    new Route("/account", "Mon compte", "/Pages/auth/account.html"),
-    new Route("/editPassword", "Modifier mot de passe", "/Pages/auth/editPassword.html"),
-    new Route("/allResa", "Vos réservations", "/Pages/reservations/allResa.html"),
-    new Route("/reserver", "Réserver", "/Pages/reservations/reserver.html"),
+    new Route("/", "Accueil", "/Pages/home.html", []),
+    new Route("/galerie", "La galerie", "/Pages/galerie.html", []),
+    new Route("/signin", "Connexion", "/Pages/auth/signin.html", ["disconnected"], "/js/auth/signin.js"),
+    new Route("/signup", "Inscription", "/Pages/auth/signup.html", ["disconnected"], "/js/auth/signup.js"),
+    new Route("/account", "Mon compte", "/Pages/auth/account.html", ["client", "admin"]),
+    new Route("/editPassword", "Modifier mot de passe", "/Pages/auth/editPassword.html", ["client", "admin"]),
+    new Route("/allResa", "Vos réservations", "/Pages/reservations/allResa.html", ["client"]),
+    new Route("/reserver", "Réserver", "/Pages/reservations/reserver.html", ["client"]),
 ];
 
 //Le titre s'affiche comme ceci : Route.titre - websitename
